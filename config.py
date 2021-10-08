292f825ae6ec84529a5d13023587945f26882655
Sidhant <sidhantj01@gmail.com>
2021-10-09 02:13:11 +0530

    changes

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\<enter-user-name>\\AppData\\Local\\Google\\Chrome\\User Data\\Auto Attend"
+# enter your windows user name in place of "<enter-user-name>"
+
+GroupName = "<GROUP-NAME-HERE>"
+# enter the group or contact name from whom you will receive the meet link
+
+Duration = 120
+# enter the class duration in minutes
+
+Message = "hola amigos"
+# enter the message you want to send in the google meet chat after joining
+# it could be your roll number , or "present mam" , or "sorry mic not working" ... or anything you want
+# enter "None" (without double quotes) if you don't want to send any message
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from config import CHROME_PROFILE_PATH
 from pygame.time import Clock
 import time
+import sys
 
 # enter the number of seconds after which you want to start looking for the meet link
 time.sleep(<enter time>)
@@ -16,9 +17,53 @@ driver.maximize_window()
 driver.get('https://web.whatsapp.com')
 
 time.sleep(5)
-driver.find_element_by_xpath('//span[@title="<enter group/contact name>"]').click()
+
+try:
+	driver.find_element_by_xpath('//span[@title="<enter group/contact name>"]').click()
+except:
+	driver.quit()
+	sys.exit()
 
 clock = Clock()
+
+def sendMessage(message):
+	chatButton = WebDriverWait(driver, 600).until(
+		EC.presence_of_element_located((By.XPATH, '//button[@aria-label="Chat with everyone"]'))
+		)
+
+	chatButton.click()
+
+	time.sleep(2)
+	textarea = driver.find_element_by_xpath('//textarea[@jsname="YPqjbf"][@aria-label="Send a message to everyone"]')
+	textarea.send_keys(message + Keys.ENTER)
+
+def joinMeet(link):
+
+	driver.get(link)
+	time.sleep(10)
+
+	try:
+		driver.find_element_by_xpath('//div[@aria-label="Turn off camera (ctrl + e)"]').click()
+		driver.find_element_by_xpath('//div[@aria-label="Turn off microphone (ctrl + d)"]').click()
+	except:
+		driver.quit()
+		sys.exit()
+
+	joinXPATH = '//span[@class="NPEfkd RveJvd snByac"][text()="Join now"]'
+	askXPATH = '//span[@class="NPEfkd RveJvd snByac"][text()="Ask to join"]'
+	joined = false
+
+	if (driver.findElement(By.XPATH(joinXPATH)).size() != 0):
+		driver.find_element_by_xpath(joinXPATH).click()
+		joined = True
+	elif (driver.findElement(By.XPATH(askXPATH)).size() != 0):
+		driver.find_element_by_xpath(askXPATH).click()
+		joined = True
+
+	if !joined:
+		driver.quit()
+		sys.exit()
+
 
 while True:
 
@@ -28,16 +73,10 @@ while True:
 	recent = messages[-1].text
 
 	if 'meet.google.com' in recent:
-		driver.get(recent)
-		time.sleep(5)
-		driver.find_element_by_xpath('//div[@aria-label="Turn off camera (ctrl + e)"]').click()
-		driver.find_element_by_xpath('//div[@aria-label="Turn off microphone (ctrl + d)"]').click()
-		driver.find_element_by_xpath('//span[@class="NPEfkd RveJvd snByac"][text()="Join now"]').click()
-		time.sleep(3)
-		driver.find_element_by_xpath('//button[@aria-label="Chat with everyone"]').click()
-		time.sleep(2)
-		textarea = driver.find_element_by_xpath('//textarea[@jsname="YPqjbf"][@aria-label="Send a message to everyone"]')
-		textarea.send_keys("<enter-your-roll-number>" + Keys.ENTER)
+
+		joinMeet(recent)
+		sendMessage("2K19/CO/378")
+		
 		break
 
 # enter the number of seconds for which you want to attend the meet
